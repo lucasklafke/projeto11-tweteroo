1d9df5f66f3a0491d19c59c989c10310149682f2
Lucas klafke <lucasnkz11@gmail.com>
2022-04-22 13:31:30 -0300

    interações com o servidor

--- index.js
+++ index.js
@@ -2,3 +2,24 @@ import cors from "cors"
 import express from "express"
 
 const app = express()
+app.use(cors())
+const users = [{
+        username: 'bobesponja',
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
+    }
+]
+const tweets = []
+
+app.listen(5000)
+
+app.post("/sign-up", (req, res) => {
+    res.send([...users])
+})
+
+app.post("/tweets",(req,res)=>{
+    res.send([...tweets])
+})
+
+app.get("/tweets", (req, res) => {
+    res.send([...tweets])
+})
